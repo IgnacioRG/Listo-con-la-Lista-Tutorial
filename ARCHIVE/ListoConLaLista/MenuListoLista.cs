51c79c0460eeb3bad8e97e71ee35dbe8371f339f
IgnacioRG <IgnacioRG@ciencias.unam.mx>
2023-11-21 12:32:04 -0600

    LcL Archive
    Recursos originales del juego.

---
+++ ARCHIVE/ListoConLaLista/MenuListoLista.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuListoLista : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Demo_Memoria");
+    }
+
+    public void Salir()
+    {
+        SceneManager.LoadScene("Menu Juegos");
+    }
+}
